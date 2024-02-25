984577da78bc7ed3286769f2c663469af80ed3b6
Paweł <pavelturczynski@gmail.com>
2024-02-25 21:12:12 +0100

    small fix

--- src/ui/Gallery.jsx
+++ src/ui/Gallery.jsx
@@ -1,6 +1,3 @@
-import LightGallery from "lightgallery/react";
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
